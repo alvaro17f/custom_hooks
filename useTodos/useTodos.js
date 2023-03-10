2df03b2e33ce852ac2899490b2328edbb59cf858
Alvaro Garcia <89421445+alvaro17f@users.noreply.github.com>
2023-03-10 19:04:42 +0100

    .

---
+++ useTodos/useTodos.js
@@ -0,0 +1,43 @@
+import { useReducer } from "react";
+import { todoReducer } from "./todoReducer";
+
+const init = () => {
+	return JSON.parse(localStorage.getItem("todos")) || [];
+};
+
+export const useTodos = (initialState = []) => {
+	const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+	const handleNewTodo = (todo) => {
+		const action = {
+			type: "[TODO] Add Todo",
+			payload: todo,
+		};
+
+		dispatch(action);
+	};
+
+	const handleToggleTodo = (id) => {
+		dispatch({
+			type: "[TODO] Toggle Todo",
+			payload: id,
+		});
+	};
+
+	const handleDeleteTodo = (id) => {
+		dispatch({
+			type: "[TODO] Remove Todo",
+			payload: id,
+		});
+	};
+
+	return {
+		...initialState,
+		todos,
+		handleDeleteTodo,
+		handleToggleTodo,
+		handleNewTodo,
+		todosCount: todos.length,
+		pendingTodosCount: todos.filter((todo) => !todo.done).length,
+	};
+};
